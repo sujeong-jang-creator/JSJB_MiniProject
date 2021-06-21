1201acabb5c8cc8df3665042c18ef464080458c6
rnaqpddl123 <83867157+rnaqpddl123@users.noreply.github.com>
2021-06-21 17:23:06 +0900

    Create item.sql

---
+++ Main_Project/Table/item.sql
@@ -0,0 +1,117 @@
+create table items (
+  i_id int,
+  i_name varchar2(40),
+  i_level varchar2(40),
+  i_price int,
+  primary key(i_id)
+  );
+
+
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(1, '잉걸불 칼', '시작', 350);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(2, '핏빌칼날', '시작', 350);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(3, '암흑의인장', '시작', 350);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(4, '도란의반지', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(5, '여신의 눈물', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(6, '영혼의 낫', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(7, '주문도둑의 검', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(8, '강철 어깨 보호대', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(9, '고대유물 방패', '시작', 400);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(10, '도란의 방패', '시작', 450);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(11, '도란의 검', '시작', 450); 
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(12, '수확의 낫', '시작', 450);
+
+
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(28, '바미의 불씨', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(29, '사라진 양피지', '서사', 1300);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(30, '흡수의 시선', '서사', 1300);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(31, '절정의 화살', '서사', 1300);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(32, '강철가시 채찍', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(33, '수정 팔 보호구', '서사', 650);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(34, '광휘의 검', '서사', 700);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(35, '키리히아이스의 파편', '서사', 700); 
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(36, '쇠사슬 조끼', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(37, '비상의 월갑', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(38, '점화석', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(39, '덤불조끼', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(40, '금지된 우상', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(41, '처형인의 대검', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(42, '망각의 구', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(43, '분노의 칼', '서사', 800);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(44, '에테르 환영', '서사', 850);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(45, '거인의 허리띠', '서사', 900);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(46, '흡혈의 낫', '서사', 900);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(47, '음전자 망토', '서사', 900);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(48, '얼음 방패', '서사', 900);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(49, '악마의 마법서', '서사', 900);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(50, '추적자의 팔목 보호대', '서사', 1000);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(51, '밴들유리 거울', '서사', 950);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(52, '곡궁', '서사', 1000);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(53, '파수꾼의 갑옷', '서사', 1000);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(54, '열정의 검', '서사', 1050);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(55, '마법공학 교류 발전기', '서사', 1050);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(56, '탐식의 망치', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(57, '온기가 필요한 자의 도끼', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(58, '콜필드의 전투 망치', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(59, '톱날 단검', '서사', 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(60, '감시하는 와드석' 1100);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(61, '티아맷', '서사', 1200);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(62, '신록의 장벽', '서사', 1000);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(63, '망령의 두건', '서사', 1250);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(64, '역병의 보석', '서사', 1250);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(65, '수은 장식띠', '서사', 1300);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(66, '주문 포식자', '서사', 1300);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(67, '최후의 속삭임', '서사', 1450);
+INSERT INTO items (i_id, i_name, i_level, i_price) VALUES
+(68, '군단의 방패', '서사', 1500);
