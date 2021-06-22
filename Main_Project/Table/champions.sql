541302bfacfaa3b9818eb059307e4dd0b6c464ab
rnaqpddl123 <83867157+rnaqpddl123@users.noreply.github.com>
2021-06-22 17:41:57 +0900

    Update champions.sql
    add 1~80

--- Main_Project/Table/champions.sql
+++ Main_Project/Table/champions.sql
@@ -14,170 +14,168 @@ CREATE TABLE champions (c_id int,
                         foreign key(s_id2) references spell2(s_id2)
                         );
 /*2009*/
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(1, '누누와 월럼프', '소년과 설인', '서포터, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(2, '라이즈', '방랑 마법사', '마법사, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(3, '마스터 이', '우주 검사', '암살자, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(4, '모르가나', '타락한 자', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(5, '사이온', '언데드 학살병기', '전사 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(6, '소라카', '별의 아이', '서포터, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(7, '시비르', '전장의 여제', '원거리, 보호막');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(8, '알리스타', '미노타우로스', '탱커, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(9, '애니', '어둠의 아이', '마법사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(10, '애쉬', '서리 궁수', '원거리, 정찰');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(11, '워윅', '피의 사냥꾼', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(12, '잭스', '무기의 달인', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(13, '케일', '정의로운 자', '전사, 치유자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(14, '트리스타나', '요들 사수', '원거리, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(15, '트위스티드 페이트', '카드의 달인', '마법사, 정찰');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(16, '티모', '날쌘 정찰병', '원거리, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(17, '피들스틱', '종말의 전조', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(18, '신지드', '미친 화학자', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(19, '질리언', '시간의 수호자', '서포터, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(20, '이블린', '학살자', '암살자, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(20, '이블린', '학살자', '암살자, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(21, '트린다미어', '야만전사 왕', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(22, '트위치', '역병 쥐', '원거리, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(23, '카서스', '죽음을 노래하는 자', '마법사, 둔화');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(24, '아무무', '슬픈 미라', '탱커, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(25, '초가스', '공허의 공포', '탱커, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(26, '람머스', '중무장 아르마딜로', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(27, '애니비아', '얼음불사조', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(28, '베이가', '악의 작은 지배자', '마법사, 기절');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(29, '카사딘', '공허의 방랑자', '암살자, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(30, '갱플랭크', '바다의 무법자', '전사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(31, '타릭', '발로란의 방패', '탱커, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(32, '말파이트', '거석의 파편', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(33, '문도 박사', '자운의 광인', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(34, '블리츠크랭크', '거대 증기 골렘', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(35, '잔나', '폭풍의 분노', '서포터, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(36, '카타리나', '사악한 칼날', '암살자, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(37, '코르키', '대담한 폭격수', '원거리, 탈출');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(38, '나서스', '사막의 관리자', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(39, '샤코', '악마 어릿광대', '암살자, 탈출');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(40, '하이머딩거', '위대한 발명가', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(41, '우디르', '정령 주술사', '전사 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(42, '니달리', '야성의 사냥꾼', '암살자, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(1, 2, 135, 9, 5, '누누와 윌럼프', '소년과 설인', '서포터, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(2, 11, 142, 10, 5, '라이즈', '방랑 마법사', '마법사, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(3, 4, 132, 9, 5, '마스터 이', '우주 검사', '암살자, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(4, 10, 152, 3, 5, '모르가나', '타락한 자', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(5, 12, 134, 10, 5, '사이온', '언데드 학살병기', '전사 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(6, 9, 153, 4, 5, '소라카', '별의 아이', '서포터, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(7, 3, 137, 7, 5, '시비르', '전장의 여제', '원거리, 보호막');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(8, 13, 151, 4, 5, '알리스타', '미노타우로스', '탱커, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(9, 1, 141, 3, 5, '애니', '어둠의 아이', '마법사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(10, 6, 132, 7, 5, '애쉬', '서리 궁수', '원거리, 정찰');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(11, 12, 148, 10, 5, '워윅', '피의 사냥꾼', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(12, 5, 148, 10, 5, '잭스', '무기의 달인', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(13, 8, 94, 10, 5, '케일', '정의로운 자', '전사, 치유자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(14, 4, 132, 7, 5, '트리스타나', '요들 사수', '원거리, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(15, 1, 142, 10, 5, '트위스티드 페이트', '카드의 달인', '마법사, 정찰');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(16, 12, 144, 3, 5, '티모', '날쌘 정찰병', '원거리, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(17, 2, 143, 9, 5, '피들스틱', '종말의 전조', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(18, 5, 144, 10, 5, '신지드', '미친 화학자', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(19, 9, 150, 3, 5, '질리언', '시간의 수호자', '서포터, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(20, 1, 145, 9, 5, '이블린', '학살자', '암살자, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(21, 6, 131, 6, 5, '트린다미어', '야만전사 왕', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(22, 8, 132, 7, 5, '트위치', '역병 쥐', '원거리, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(23, 3, 140, 9, 5, '카서스', '죽음을 노래하는 자', '마법사, 둔화');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(24, 13, 135, 9, 5, '아무무', '슬픈 미라', '탱커, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(25, 12, 134, 10, 5, '초가스', '공허의 공포', '탱커, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(26, 13, 135, 9, 5, '람머스', '중무장 아르마딜로', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(27, 1, 142, 10, 5, '애니비아', '얼음불사조', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(28, 10, 142, 3, 5, '베이가', '악의 작은 지배자', '마법사, 기절');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(29, 7, 142, 10, 5, '카사딘', '공허의 방랑자', '암살자, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(30, 12, 148, 10, 5, '갱플랭크', '바다의 무법자', '전사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(31, 14, 151, 4, 5, '타릭', '발로란의 방패', '탱커, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(32, 10, 136, 10, 5, '말파이트', '거석의 파편', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(33, 12, 136, 10, 5, '문도 박사', '자운의 광인', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(34, 13, 151, 3, 5, '블리츠크랭크', '거대 증기 골렘', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(35, 10, 150, 3, 5, '잔나', '폭풍의 분노', '서포터, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(36, 1, 143, 3, 5, '카타리나', '사악한 칼날', '암살자, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(37, 7, 148, 10, 5, '코르키', '대담한 폭격수', '원거리, 탈출');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(38, 16, 148, 10, 5, '나서스', '사막의 관리자', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(39, 4, 137, 3, 9, '샤코', '악마 어릿광대', '암살자, 탈출');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(40, 10, 140, 10, 5, '하이머딩거', '위대한 발명가', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(41, 5, 135, 9, 5, '우디르', '정령 주술사', '전사 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(42, 3, 145, 9, 5, '니달리', '야성의 사냥꾼', '암살자, 전사');
 /*2010*/
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(43, '뽀삐', '망치의 수호자', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(44, '그라가스', '술취한 난동꾼', '전사, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(45, '판테온', '불굴의 창', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(46, '모데카이저', '강철의 망령', '전사, 보호막');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(47, '이즈리얼', '방탕한 탐험가', '원거리, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(48, '쉔', '황혼의 눈', '탱커, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(49, '케넨', '폭풍의 심장', '마법사, 원거리');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(50, '가렌', '데마시아의 힘', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(51, '아칼리', '그림자의 권', '암살자, 은신');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(52, '말자하', '공허의 예언자', '마법사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(53, '올라프', '광전사', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(54, '코그모', '심연의 아귀', '원거리, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(55, '신 짜오', '데마시아의 호위무사', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(56, '블라디미르', '진홍빛 사신', '마법사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(57, '갈리오', '위대한 석상', '마법사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(58, '우르곳', '살상 병기', '전사, 원거리');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(59, '미스 포츈', '현상금 사냥꾼', '원거리, 둔화');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(60, '소나', '현의 명인', '서포터, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(61, '스웨인', '전략의 대가', '마법사, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(62, '럭스', '광명의 소녀', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(63, '르블랑', '환술사', '암살자, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(64, '이렐리아', '칼날의 의지', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(65, '트런들', '트롤 왕', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(66, '카시오페아', '독사의 포옹', '마법사, 둔화');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(43, 12, 136, 10, 5, '뽀삐', '망치의 수호자', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(44, 2, 145, 9, 5, '그라가스', '술취한 난동꾼', '전사, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(45, 5, 138, 3, 5, '판테온', '불굴의 창', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(46, 5, 144, 10, 5, '모데카이저', '강철의 망령', '전사, 보호막');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(47, 5, 148, 7, 5, '이즈리얼', '방탕한 탐험가', '원거리, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(48, 12, 134, 10, 5, '쉔', '황혼의 눈', '탱커, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(49, 9, 143, 10, 5, '케넨', '폭풍의 심장', '마법사, 원거리');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(50, 5, 147, 10, 5, '가렌', '데마시아의 힘', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(51, 5, 144, 3, 5, '아칼리', '그림자의 권', '암살자, 은신');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(52, 9, 140, 10, 5, '말자하', '공허의 예언자', '마법사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(53, 5, 146, 9, 5, '올라프', '광전사', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(54, 6, 82, 7, 5, '코그모', '심연의 아귀', '원거리, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(55, 4, 138, 9, 5, '신 짜오', '데마시아의 호위무사', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(56, 9, 143, 3, 6, '블라디미르', '진홍빛 사신', '마법사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(57, 13, 143, 10, 5, '갈리오', '위대한 석상', '마법사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(58, 8, 147, 10, 5, '우르곳', '살상 병기', '전사, 원거리');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(59, 5, 132, 7, 5,  '미스 포츈', '현상금 사냥꾼', '원거리, 둔화');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(60, 9, 150, 3, 5, '소나', '현의 명인', '서포터, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(61, 1, 140, 3, 5, '스웨인', '전략의 대가', '마법사, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(62, 10, 141, 3, 5, '럭스', '광명의 소녀', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(63, 1, 141, 3, 5, '르블랑', '환술사', '암살자, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(64, 5, 85, 10, 5, '이렐리아', '칼날의 의지', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(65, 8, 148, 9, 5, '트런들', '트롤 왕', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(66, 5, 140, 10, 5, '카시오페아', '독사의 포옹', '마법사, 둔화');
 /*2011*/
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(67, '케이틀린', '필트오버의 보안관', '원거리, 탈출');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(68, '레넥톤', '사막의 도살자', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(69, '카르마', '깨우친 자', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(70, '마오카이', '뒤틀린 나무 정령', '탱커, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(71, '자르반 4세', '데마시아의 귀감', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(72, '녹턴', '영원한 악몽', '암살자, 전사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(73, '리 신', '눈 먼 수도승', '전사, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(74, '브랜드', '타오르는 복수', '마법사, 기절');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(75, '럼블', '기계 악동', '전사, 마법사');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(76, '베인', '어둠 사냥꾼', '원거리, 암살자');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(77, '오리아나', '시계태엽 소녀', '마법사, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(78, '요릭', '망자의 길잡이', '전사, 탱커');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(79, '레오나', '여명의 빛', '탱커, 서포터');
-INSERT INTO champions (c_id, c_name, c_title, c_tags) VALUES
-(80, '오공', '원숭이 왕', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(67, 7, 131, 7, 5, '케이틀린', '필트오버의 보안관', '원거리, 탈출');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(68, 5, 147, 10, 5, '레넥톤', '사막의 도살자', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(69, 10, 150, 3, 5, '카르마', '깨우친 자', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(70, 12, 136, 10, 5, '마오카이', '뒤틀린 나무 정령', '탱커, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(71, 5, 146, 9, 5, '자르반 4세', '데마시아의 귀감', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(72, 5, 147, 10, 5, '녹턴', '영원한 악몽', '암살자, 전사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(73, 5, 146, 9, 5, '리 신', '눈 먼 수도승', '전사, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(74, 3, 140, 3, 5, '브랜드', '타오르는 복수', '마법사, 기절');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(75, 3, 145, 9, 5, '럼블', '기계 악동', '전사, 마법사');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(76, 8, 132, 7, 5, '베인', '어둠 사냥꾼', '원거리, 암살자');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(77, 11, 141, 10, 5, '오리아나', '시계태엽 소녀', '마법사, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(78, 12, 148, 10, 5, '요릭', '망자의 길잡이', '전사, 탱커');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(79, 13, 151, 3, 5, '레오나', '여명의 빛', '탱커, 서포터');
+INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
+(80, 5, 148, 10, 5, '오공', '원숭이 왕', '전사, 탱커');
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
 (81, 11, 135, 9, 5, '스카너', '수정 선봉장', '전사, 탱커');
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
@@ -212,7 +210,7 @@ INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) V
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
 (96, 11, 135, 5, 9, '헤카림', '전쟁의 전조', '전사, 탱커');
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
-(97, 4, 139, 5, 7 '바루스', '응징의 화살', '원거리, 마법사');
+(97, 4, 139, 5, 7, '바루스', '응징의 화살', '원거리, 마법사');
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
 (98, 5, 147, 5, 6, '다리우스', '녹서스의 실력자','전사, 탱커');
 INSERT INTO champions (c_id, r_id, i_id, s_id, s_id2, c_name, c_title, c_tags) VALUES
